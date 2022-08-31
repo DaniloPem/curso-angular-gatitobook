277226040435046f9a43467c261bb3c318b321f8
Danilo Pezo <danilo_97pem@hotmail.com>
2022-08-31 16:43:16 -0300

    Melhorado o código usando rxjs

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,3 +1,4 @@
+import { switchMap, Observable } from 'rxjs';
 import { AnimaisService } from './../animais.service';
 import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
 import { Animais } from './../animais';
@@ -10,17 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais!: Animais;
+  animais$!: Observable<Animais>;
 
   constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) { }
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      const username = usuario.name ?? '';
-      this.animaisService.listaDoUsuario(username).subscribe((animais) =>{
-        this.animais = animais;
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
       })
-    })
+    )
   }
 
 }
